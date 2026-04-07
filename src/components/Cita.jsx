86f1171228b58658b29442504647c33cb604e0f0
SANTIAGO NICOLÁS HONG <49189314@ort>
2026-04-07 08:41:50 -0300

    prompts

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -1,12 +1,12 @@
-function Cita() {
+function Cita(props) {
 
     return (
         <div className="cita">
-            <p>Mascota: <span>Nina</span></p>
-            <p>Dueño: <span>Martin</span></p>
-            <p>Fecha: <span>2021-08-05</span></p>
-            <p>Hora: <span>08:20</span></p>
-            <p>Sintomas: <span>Le duele la pierna</span></p>
+            <p>Mascota: <span>{props.mascota}</span></p>
+            <p>Dueño: <span>{props.dueno}</span></p>
+            <p>Fecha: <span>{props.fecha}</span></p>
+            <p>Hora: <span>{props.hora}</span></p>
+            <p>Sintomas: <span>{props.sintomas}</span></p>
             <button className="button elimnar u-full-width">Eliminar ×</button>
         </div>
     )
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -5,10 +5,10 @@ function Listado() {
     return (
         <section>
             <h2>Administra tus citas</h2>
-            <section>
-                <Cita />
-                <Cita />
-                <Cita />
+            <section> 
+                <Cita mascota="Nina" dueno="Martín" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna"/>
+                <Cita mascota="Sifon" dueno="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho"/>
+                <Cita mascota="Floki" dueno="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo"/>
             </section>
         </section>
     )
